36a9a0e789073b830d0e8fd5d20a5397c5d4b9b7
zakia1230 <zakia10parween1423@gmail.com>
2022-03-29 21:26:29 +0530

    Second Commit

--- Project_java/src/com/cg/client/Test.java
+++ Project_java/src/com/cg/client/Test.java
@@ -4,6 +4,7 @@ public class Test {
 
 	public static void main (String args[])
 	{
-		System.out.println("This a is a test class");
+		System.out.println("This a is a sample class project");
+		System.out.println("This a is a project");
 	}
 }
